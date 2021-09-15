653296be46511738a29ece68f7c083d1e6597497
Tuba05 <bbence925@gmail.com>
2021-09-15 13:24:17 +0200

    add monsters package and class with basic fields

--- src/main/java/com/codecool/dungeoncrawl/logic/Cell.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/Cell.java
@@ -38,6 +38,8 @@ public class Cell implements Drawable {
         this.item = item;
     }
 
+
+
     public Item getItem() {
         return item;
     }
---
+++ src/main/java/com/codecool/dungeoncrawl/logic/monsters/Monster.java
@@ -0,0 +1,11 @@
+package com.codecool.dungeoncrawl.logic.monsters;
+
+import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.Drawable;
+
+public abstract class Monster implements Drawable {
+    private String name;
+    private int health;
+    private Cell cell;
+
+}
