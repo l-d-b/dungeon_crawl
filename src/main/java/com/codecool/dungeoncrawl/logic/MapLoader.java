d7961b363175204234fba7b5af10b32b4e40858c
Tuba05 <bbence925@gmail.com>
2021-09-15 13:49:37 +0200

    add ghost cell in MapLoader

--- src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
@@ -60,6 +60,9 @@ public class MapLoader {
                         case 'g':
                             cell.setType(CellType.GOLEM);
                             break;
+                        case '¤':
+                            cell.setType(CellType.GHOST);
+                            break;
                         default:
                             throw new RuntimeException("Unrecognized character: '" + line.charAt(x) + "'");
                     }
