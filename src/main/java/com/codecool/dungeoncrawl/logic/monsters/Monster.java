3b9f4072804fad7c8b8541cc5863e4b8a0a95468
Tuba05 <bbence925@gmail.com>
2021-09-15 13:25:28 +0200

    add monster constructor

--- src/main/java/com/codecool/dungeoncrawl/logic/monsters/Monster.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/monsters/Monster.java
@@ -8,4 +8,10 @@ public abstract class Monster implements Drawable {
     private int health;
     private Cell cell;
 
+    public Monster(String name, int health, Cell cell) {
+        this.name = name;
+        this.health = health;
+        this.cell = cell;
+        this.cell.setMonster(this);
+    }
 }
