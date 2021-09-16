2fda281650c270e3587170b11584d266ea35dd2c
Tuba05 <bbence925@gmail.com>
2021-09-16 10:33:09 +0200

    add RedPotion class and its constructor

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/RedPotion.java
@@ -0,0 +1,9 @@
+package com.codecool.dungeoncrawl.logic.items;
+
+import com.codecool.dungeoncrawl.logic.Cell;
+
+public class RedPotion extends Item{
+    public RedPotion(Cell cell) {
+        super(cell);
+    }
+}
