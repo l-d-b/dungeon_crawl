7351fd6526bbb285c6a6ae54070b364552818c81
Tuba05 <bbence925@gmail.com>
2021-09-19 18:22:43 +0200

    [#37] refactor Cell and GameMap

--- src/main/java/com/codecool/dungeoncrawl/logic/GameMap.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/GameMap.java
@@ -1,7 +1,6 @@
 package com.codecool.dungeoncrawl.logic;
 
 import com.codecool.dungeoncrawl.logic.actors.Player;
-import com.codecool.dungeoncrawl.logic.items.Item;
 import com.codecool.dungeoncrawl.logic.monsters.Monster;
 
 public class GameMap {
