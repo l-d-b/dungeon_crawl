d2ef7efc5841caefa52eeedb7d7b360ecc4376e7
Tuba05 <bbence925@gmail.com>
2021-09-28 13:03:28 +0200

    refactor after merge

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -46,28 +46,5 @@ public class Player extends Actor {
 
         }
         getCell().setType(CellType.FLOOR);
-
-public class Player extends Actor {
-    private String name;
-
-    public Player(Cell cell) {
-        super(cell);
-    }
-
-    public Player(Cell cell, String name) {
-        super(cell);
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTileName() {
-        return "player";
     }
 }
